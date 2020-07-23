07e7e911447658fe8033b72103637dc4c2b9ba27
zsakvo <gdwqsp@gmail.com>
2020-07-23 16:10:51 +0800

    init code

---
+++ src/plugins/ant-design-vue.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import { Pagination, Button } from 'ant-design-vue'
+import { LocaleProvider } from 'ant-design-vue'
+Vue.component(LocaleProvider.name, LocaleProvider)
+Vue.component(Pagination.name, Pagination)
+Vue.component(Button.name, Button)
