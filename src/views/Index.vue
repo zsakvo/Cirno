6dce143ef42ec1ad0389c6508e584867ff720af8
zsakvo <gdwqsp@gmail.com>
2020-10-26 23:50:40 +0800

    fix with new shelf page

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -7,29 +7,29 @@
       </div>
       <div class="options">
         <div class="change-shelf option-icon">
-          <i class="ri-stack-line"></i>
+          <i class="ri-loader-4-line"></i>
         </div>
         <div class="change-shelf option-icon">
-          <i class="ri-keynote-line"></i>
+          <i :class="{ 'ri-calendar-check-line': checkIn, 'ri-calendar-line': !checkIn }"></i>
         </div>
         <div class="change-shelf option-icon">
           <i class="ri-search-2-line"></i>
         </div>
-        <img class="avatar" :src="avatar ? avatar : require('@/assets/d_avatar.jpg')" alt="用户头像" />
+        <div class="change-shelf option-icon">
+          <i class="ri-settings-line"></i>
+        </div>
+        <!-- <img class="avatar" :src="avatar ? avatar : require('@/assets/d_avatar.jpg')" alt="用户头像" /> -->
       </div>
     </div>
     <div class="books-wrapper">
       <div class="books">
-        <div class="book">
-          <img
-            class="book-cover"
-            src="http://c1.kuangxiangit.com/uploads/allimg/c180824/24-08-18170353-43659-100076156.jpg"
-            alt=""
-          />
-          <div class="book-name">神的调色盘</div>
+        <div class="book" v-for="book in book_list" :key="book.id" @click="gotoBook(book)">
+          <img class="book-cover" :src="book.book_info.cover" />
+          <div class="book-name">{{ book.book_info.book_name }}</div>
         </div>
       </div>
     </div>
+    <!-- <div class="bottom-bar"></div> -->
   </div>
 </template>
 
@@ -39,11 +39,13 @@ export default {
     return {
       book_list: [],
       currentShelfId: 0,
-      avatar: this.$store.state.reader_info.avatar_thumb_url
+      avatar: this.$store.state.reader_info.avatar_thumb_url,
+      checkIn: false
     }
   },
   created() {
     this.getInfo()
+    this.checkSign()
     this.$get({
       url: '/bookshelf/get_shelf_list',
       urlParas: {}
@@ -80,6 +82,18 @@ export default {
         }
       )
     },
+    async checkSign() {
+      let sign_record_list = await this.$get({
+        url: '/task/get_sign_record'
+      })
+      sign_record_list = sign_record_list.data.sign_record_list
+      console.log(sign_record_list)
+      let date = new Date()
+      let today = date.getDay()
+      let dayArr = [6, 0, 1, 2, 3, 4, 5]
+      this.checkIn = sign_record_list[dayArr[today]]['is_signed'] !== 1
+      console.log(this.checkIn)
+    },
     async getBooks() {
       let book_list = await this.$get({
         url: '/bookshelf/get_shelf_book_list_new',
@@ -91,6 +105,15 @@ export default {
         }
       })
       this.book_list = book_list.data.book_list
+    },
+    gotoBook(book) {
+      this.$router.push({
+        name: 'Book',
+        query: {
+          bid: book.book_info.book_id,
+          cid: book.last_read_chapter_id
+        }
+      })
     }
   }
 }
@@ -99,9 +122,11 @@ export default {
 <style lang="less" scoped>
 .index-wrapper {
   width: 80%;
+  height: 100%;
   margin: 0 auto;
-  padding: 24px 0;
   .top-bar {
+    z-index: 200;
+    padding: 36px 0;
     position: fixed;
     width: 80%;
     background: #fff;
@@ -119,7 +144,8 @@ export default {
       display: flex;
       justify-content: space-between;
       align-items: center;
-      width: 20%;
+      // width: 20%;
+      width: 220px;
       .avatar {
         width: 48px;
         border-radius: 50%;
@@ -131,22 +157,43 @@ export default {
   }
 
   .books-wrapper {
-    padding-top: 72px;
-    height: 800px;
-    width: 100%;
+    padding-top: 120px;
+    position: absolute;
+    top: 0;
+    width: 80%;
+    padding-bottom: 36px;
+    background: #fff;
     .books {
-      height: 800px;
       width: 100%;
+      grid-row-gap: 36px;
+      grid-column-gap: 32px;
+      display: grid;
+      grid-template-columns: repeat(auto-fill, 108px);
+      justify-content: space-between;
       .book {
         width: 108px;
+        cursor: pointer;
         .book-cover {
           width: 100%;
+          height: 144px;
         }
         .book-name {
           margin-top: 8px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
         }
       }
     }
+  }
+
+  .bottom-bar {
+    height: 36px;
+    width: 80%;
+    position: fixed;
+    bottom: 0;
+    background: #fff;
+    z-index: 20;
   }
 }
 </style>
